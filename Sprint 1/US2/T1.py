3bb011a530fc66273e023f785cd772f03b82df36
BerkayAtakli <9023046@student.zadkine.nl>
2026-09-14 15:29:19 +0200

    Update T1.py

--- Sprint 1/US2/T1.py
+++ Sprint 1/US2/T1.py
@@ -4,4 +4,10 @@ flight_height = 15.5
 is_flying = True
 extra_cargo = None
 
-print(drone_name)
+print(drone_name, type(drone_name))
+print(battery_percentage, type(battery_percentage))
+print(flight_height, type(flight_height))
+print(is_flying, type(is_flying))
+print(extra_cargo, type(extra_cargo))
+
+# We gebruiken None omdat 0 een gewicht zou betekenen, en None betekent dat er helemaal geen cargo is.
