20f4a3fdf6c2af3fb943c55fe98b7fb434de9b3c
BerkayAtakli <9023046@student.zadkine.nl>
2026-09-14 15:09:42 +0200

    ;

--- Sprint 1/US1/T3.py
+++ Sprint 1/US1/T3.py
@@ -3,5 +3,12 @@ MAX_HEIGHT = 500
 current_battery = None
 is_flying = False
 
+print(type(drone_name))
+print(type(MAX_HEIGHT))
+print(type(current_battery))
+print(type(is_flying))
+
 def check_drone():
     print(drone_name)
+
+check_drone()
---
+++ Sprint 1/US2/T1.py
@@ -0,0 +1,7 @@
+drone_name = "Berkay".upper()
+battery_percentage = 100
+flight_height = 15.5
+is_flying = True
+extra_cargo = None
+
+print(drone_name)
