2ff0bafaef74b610f23078d613c77659268aeadb
BerkayAtakli <9023046@student.zadkine.nl>
2026-09-14 16:08:48 +0200

    Create T2.py

---
+++ Sprint 1/US2/T2.py
@@ -0,0 +1,16 @@
+battery_percentage = (input("Battery_percentage: "))
+percentage_cost_per_minute_to_fly = (input("percentage_cost_per_minute_to_fly: "))
+
+# flight_time = battery / percentage_cost_per_minute_to_fly
+# TypeError: unsupported operand type(s) for /: 'str' and 'str'
+
+battery_percentage = int(battery_percentage)
+percentage_cost_per_minute = int(percentage_cost_per_minute_to_fly)
+
+print(type(battery_percentage))
+print(type(percentage_cost_per_minute_to_fly))
+
+flight_time = battery_percentage / percentage_cost_per_minute_to_fly
+print("Remaining flight time", flight_time)
+
+
