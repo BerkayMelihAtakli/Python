55b24d8341c6cd2bff6de54fe15a88558a16738e
BerkayAtakli <9023046@student.zadkine.nl>
2026-09-15 15:37:38 +0200

    Create T3.py

---
+++ Sprint 1/US2/T3.py
@@ -0,0 +1,10 @@
+battery = 80
+
+# omslachtig, met plakken en casten
+print("Battery: " + str(battery) + "%")
+
+# met komma's — let op de extra spaties in de uitvoer
+print("Battery:", battery, "%")     # Battery: 80 %
+
+# de f-string
+print(f"Battery: {battery}%")       # Battery: 80%
