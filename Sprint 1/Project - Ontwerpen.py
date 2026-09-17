7b78056864f4cda7fd608d52501f163e1e68ec76
BerkayAtakli <9023046@student.zadkine.nl>
2026-09-17 12:07:39 +0200

    Update Project - Ontwerpen.py

--- Sprint 1/Project - Ontwerpen.py
+++ Sprint 1/Project - Ontwerpen.py
@@ -1,23 +1,19 @@
 from djitellopy import Tello
 
-tello = Tello()
-tello.connect()
-
-time.sleep(2)
+tello = Tello(); tello.connect()
 
 battery = tello.get_battery()
 temperature = tello.get_temperature()
 flight_time = tello.get_flight_time()
 
-assert isinstance(battery, int), "Battery must be an integer"
-assert isinstance(temperature, int), "Temperature must be an integer"
-assert isinstance(flight_time, int), "Flight time must be an integer"
+assert isinstance(battery, int)
+assert isinstance(temperature, int)
+assert isinstance(flight_time, int)
 
 if battery > 20:
     status = "GO ✅"
 else:
     status = "NO-GO ❌"
-
 
 print("-" * 30)
 print("TELLO STATUS REPORT")
@@ -27,5 +23,7 @@ print(f"{'Temperature':<15}: {temperature} C")
 print(f"{'Flight Time':<15}: {flight_time}")
 print(f"{'Status': <15}: {status}")
 print("-" * 30)
+
+
 
 tello.end()
