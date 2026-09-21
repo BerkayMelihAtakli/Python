a619fabd92d4887cbbf47fe22c130d9fc1c404b9
BerkayMelihAtakli <9023046@student.zadkine.nl>
2026-09-21 22:25:06 +0200

    .

--- Sprint 1/US2/T3.py
+++ Sprint 1/US2/T3.py
@@ -1,10 +1,17 @@
-battery = 80
+disk_free = float(input("Disk free percentage: "))
+consumption_per_hour = float(input("Consumption per hour: "))
 
-# omslachtig, met plakken en casten
-print("Battery: " + str(battery) + "%")
 
-# met komma's — let op de extra spaties in de uitvoer
-print("Battery:", battery, "%")     # Battery: 80 %
+total_hours = disk_free / consumption_per_hour
 
-# de f-string
-print(f"Battery: {battery}%")       # Battery: 80%
+# Normale deling (/) geeft een kommagetal (zoals 12.3), terwijl // (floor division) alleen het hele getal overhoudt (12)
+
+
+total_minutes = total_hours * 60
+
+
+hours = total_minutes / 60
+minutes = total_minutes % 60
+
+
+print(f"web01 | disk free  {disk_free}% |  log time {hours}h {minutes}min")
